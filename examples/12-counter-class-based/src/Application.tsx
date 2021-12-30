866e35fa2846c02283471778aa6ed928c55bffa7
Peter He <peter@mathspathway.com>
2021-12-30 11:40:14 +1100

    added typing to class component and input event which is ChangeEvent<HTMLInputElement>

--- examples/12-counter-class-based/src/Application.tsx
+++ examples/12-counter-class-based/src/Application.tsx
@@ -1,21 +1,54 @@
 import { Component } from 'react';
 
-class Counter extends Component {
+type CounterProps = {
+  incident: string;
+};
+
+type CounterState = {
+  count: number;
+};
+class Counter extends Component<CounterProps, CounterState> {
+  state: CounterState = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState(({ count }) => ({ count: count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState(({ count }) => ({ count: count - 1 }));
+  };
+
+  reset = () => {
+    this.setState({ count: 0 });
+  };
+
+  changeCount = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ count: +event.target.value });
+  };
+
   render() {
+    const { incident } = this.props;
+    const { count } = this.state;
+
     return (
       <main className="Counter">
-        <h1>Days Since Last Incident</h1>
-        <p className="count">0</p>
+        <h1>Days Since Last {incident}</h1>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Reset</button>
-          <button>Decrement</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.reset}>Reset</button>
+          <button onClick={this.decrement}>Decrement</button>
         </section>
         <section className="controls">
-          <form onSubmit={() => {}}>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+          >
             <label htmlFor="set-to">Set Count</label>
             <input id="set-to" type="number" />
-            <input type="submit" />
           </form>
         </section>
       </main>
