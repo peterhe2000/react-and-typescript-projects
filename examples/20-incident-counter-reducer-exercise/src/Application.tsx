aa40b319933a9bf2a1179d23fc5c8d582a256584
Peter He <peter@mathspathway.com>
2021-12-31 18:20:55 +1100

    added union type action to force type safe of optional payload

--- examples/20-incident-counter-reducer-exercise/src/Application.tsx
+++ examples/20-incident-counter-reducer-exercise/src/Application.tsx
@@ -1,12 +1,46 @@
+import { useReducer } from 'react';
+
+type BasicCounterAction = {
+  type: 'INCREMENT' | 'DECREMENT';
+};
+
+type SetCounterAction = {
+  type: 'SET';
+  payload: number;
+};
+
+type BetterAction = BasicCounterAction | SetCounterAction;
+
+type CounterState = {
+  value: number;
+};
+
+const reducer = (state: CounterState, action: BetterAction) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value - 1 };
+    case 'SET':
+      return { value: action.payload };
+  }
+};
+
 const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  const increment = () => dispatch({ type: 'INCREMENT' });
+  const decrement = () => dispatch({ type: 'DECREMENT' });
+  const reset = () => dispatch({ type: 'SET', payload: 0 });
+
   return (
     <main className="Counter">
       <h1>Days Since Last Incident</h1>
-      <p className="count">0</p>
+      <p className="count">{state.value}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={decrement}>Decrement</button>
       </section>
     </main>
   );
