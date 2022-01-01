71cbfc63d939eb6d4724f2ac0b8798aeb789f5a3
Peter He <peter@mathspathway.com>
2022-01-02 10:11:54 +1100

    added type for higher order component

--- examples/28-character-card-with-utility-types/src/Application.tsx
+++ examples/28-character-card-with-utility-types/src/Application.tsx
@@ -5,21 +5,37 @@ import { CharacterType, fetchCharacter } from './characters';
 import { Loading } from './Loading';
 import { CharacterInformation } from './CharacterInformation';
 
+type WithCharacterProps = {
+  character: CharacterType;
+};
+
+// use function declaration is better for typing of generic since this component can be reuse withAnyComponentWant use character.
+function withCharacter<T>(Component: React.ComponentType<T>) {
+  // props component that originally took and omitting those we know we are going to pass in.
+  return (props: Omit<T, keyof WithCharacterProps>) => {
+    const [character, setCharacter] = React.useState<CharacterType | null>(
+      null
+    );
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+      fetchCharacter().then((c) => {
+        setCharacter(c);
+        setLoading(false);
+      });
+    }, []);
+
+    if (loading) return <Loading />;
+    return <Component {...(props as T)} character={character} />;
+  };
+}
+
+const CharacterInformationWithCharacter = withCharacter(CharacterInformation);
+
 const Application = () => {
-  const [character, setCharacter] = React.useState<CharacterType | null>(null);
-  const [loading, setLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetchCharacter().then((c) => {
-      setCharacter(c);
-      setLoading(false);
-    });
-  }, []);
-
   return (
     <main>
-      {loading && <Loading />}
-      {character && <CharacterInformation character={character} />}
+      <CharacterInformationWithCharacter />
     </main>
   );
 };
