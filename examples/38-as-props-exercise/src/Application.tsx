c67850d6b6f54d6a5019e3c7f61508533f976973
Peter He <peter@mathspathway.com>
2022-01-04 15:37:34 +1100

    polymorphic components typing

--- examples/38-as-props-exercise/src/Application.tsx
+++ examples/38-as-props-exercise/src/Application.tsx
@@ -1,20 +1,34 @@
 import * as React from 'react';
 
-type TextProps = {
+type TextOwnProps<E extends React.ElementType = React.ElementType> = {
   children: string;
+  as?: E;
 };
+
+// Take everything as HTML element, omit the things that are in TextOwnProps and then fill in our own TextProps
+// Get all the props of that element and then take out everything in TextOwnProps
+type TextProps<E extends React.ElementType> = TextOwnProps<E> &
+  Omit<React.ComponentProps<E>, keyof TextOwnProps>;
 
 const exampleText =
   'When I was born, the name for what I was did not exist. They called me nymph, assuming I would be like my mother and aunts and thousand cousins. Least of the lesser goddesses, our powers were so modest they could scarcely ensure our eternities. We spoke to fish and nurtured flowers, coaxed drops from the clouds or salt from the waves. That word, nymph, paced out the length and breadth of our futures.';
 
-function Text({ children }: TextProps) {
-  return <div>{children}</div>;
+const defaultElement = 'div';
+
+function Text<E extends React.ElementType = typeof defaultElement>({
+  children,
+  as,
+  ...rest
+}: TextProps<E>) {
+  const TagName = as || defaultElement; // if has 'as', use as as TagName. Otherwise use defaultElement in this case it is 'div'
+  return <TagName>{children}</TagName>;
 }
 
 const Application = () => {
   return (
     <main>
       <Text>{exampleText}</Text>
+      {/* <Text as="label" htmlFor="id">{exampleText}</Text> works */}
     </main>
   );
 };
