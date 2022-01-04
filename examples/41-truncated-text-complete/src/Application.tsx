9599fb5d0d93a3e67ee35177696e4b06c8a7c4b3
Peter He <peter@mathspathway.com>
2022-01-04 17:55:57 +1100

    overload function typing

--- examples/41-truncated-text-complete/src/Application.tsx
+++ examples/41-truncated-text-complete/src/Application.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 
 type TextProps = {
   children: string;
-  truncate?: boolean;
-  expanded?: boolean;
 };
+
+type NoTruncateTextProps = TextProps & { truncate?: false };
+type TruncateTextProps = TextProps & { truncate: true; expanded?: boolean };
 
 const exampleText =
   'When I was born, the name for what I was did not exist. They called me nymph, assuming I would be like my mother and aunts and thousand cousins. Least of the lesser goddesses, our powers were so modest they could scarcely ensure our eternities. We spoke to fish and nurtured flowers, coaxed drops from the clouds or salt from the waves. That word, nymph, paced out the length and breadth of our futures.';
@@ -12,7 +13,17 @@ const exampleText =
 const truncateString = (string: string, length = 100) =>
   string.slice(0, length) + '…';
 
-function Text({ children, truncate = false, expanded = false }: TextProps) {
+// Three variant of Text function which allow
+// 1. Optional truncate, Optional expanded
+// 2. truncate is set to false
+// 3. if truncate is true (cant optional), expanded is optional (if truncate is not exist, this function cant take expand)
+function Text(props: NoTruncateTextProps): JSX.Element;
+function Text(props: TruncateTextProps): JSX.Element;
+function Text({
+  children,
+  truncate = false,
+  expanded = false
+}: TextProps & { truncate?: boolean; expanded?: boolean }) {
   const shouldTruncate = truncate && !expanded;
   return (
     <div aria-expanded={!!expanded}>
